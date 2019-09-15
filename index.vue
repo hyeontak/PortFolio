a3d55d0e029fcc0c89435b2973bb8544da1bd5e9
leehyeontak <lht1711@naver.com>
2019-09-15 15:48:36 +0900

    test

---
+++ index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app" class="app">
+        <h1>hear</h1>
+    </div>
+</template>
+<script>
+export default {
+    name:'App',
+    data(){
+        return{}
+    },
+    
+}
+</script>
+<style lang="scss">
+
+</style>
